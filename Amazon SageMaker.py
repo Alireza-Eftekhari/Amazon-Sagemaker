74048e1f3b9041d8d84e0897c5ef35d8c3e42c04
Alireza Eftekhari <114701262+Alireza-Eftekhari@users.noreply.github.com>
2023-07-01 14:43:18 -0700

    Update and rename Untitled.py to Amazon SageMaker.py

--- Amazon SageMaker.py
+++ Amazon SageMaker.py
@@ -1,4 +1,3 @@
-
 # import libraries
 import boto3, re, sys, math, json, os, sagemaker, urllib.request
 from sagemaker import get_execution_role
@@ -21,9 +20,6 @@ xgboost_container = sagemaker.image_uris.retrieve("xgboost", my_region, "latest"
 print("Success - the MySageMakerInstance is in the " + my_region + " region. You will use the " + xgboost_container + " container for your SageMaker endpoint.")
 
 
-# In[6]:
-
-
 bucket_name = 'englandpredictionsocceris635group' # <--- CHANGE THIS VARIABLE TO A UNIQUE NAME FOR YOUR BUCKET
 s3 = boto3.resource('s3')
 try:
@@ -34,9 +30,6 @@ try:
     print('S3 bucket created successfully')
 except Exception as e:
     print('S3 error: ',e)
-
-
-# In[7]:
 
 
 try:
@@ -52,42 +45,23 @@ except Exception as e:
     print('Data load error: ',e)
 
 
-# In[8]:
-
-
 train_data, test_data = np.split(model_data.sample(frac=1, random_state=1729), [int(0.7 * len(model_data))])
 print(train_data.shape, test_data.shape)
-
-
-# In[10]:
 
 
 pd.concat([train_data['FTR_H'], train_data.drop(['FTR_D', 'FTR_H'], axis=1)], axis=1).to_csv('train.csv', index=False, header=False)
 boto3.Session().resource('s3').Bucket(bucket_name).Object(os.path.join(prefix, 'train/train.csv')).upload_file('train.csv')
 s3_input_train = sagemaker.inputs.TrainingInput(s3_data='s3://{}/{}/train'.format(bucket_name, prefix), content_type='csv')
 
-
-# In[11]:
-
-
 sess = sagemaker.Session()
 xgb = sagemaker.estimator.Estimator(xgboost_container,role, instance_count=1, instance_type='ml.m4.xlarge',output_path='s3://{}/{}/output'.format(bucket_name, prefix),sagemaker_session=sess)
 xgb.set_hyperparameters(max_depth=5,eta=0.2,gamma=4,min_child_weight=6,subsample=0.8,silent=0,objective='binary:logistic',num_round=100)
 
 
-# In[12]:
-
-
 xgb.fit({'train': s3_input_train})
 
 
-# In[13]:
-
-
 xgb_predictor = xgb.deploy(initial_instance_count=1,instance_type='ml.m4.xlarge')
-
-
-# In[14]:
 
 
 from sagemaker.serializers import CSVSerializer
@@ -99,9 +73,6 @@ predictions_array = np.fromstring(predictions[1:], sep=',') # and turn the predi
 print(predictions_array.shape)
 
 
-# In[15]:
-
-
 cm = pd.crosstab(index=test_data['FTR_H'], columns=np.round(predictions_array), rownames=['Observed'], colnames=['Predicted'])
 tn = cm.iloc[0,0]; fn = cm.iloc[1,0]; tp = cm.iloc[1,1]; fp = cm.iloc[0,1]; p = (tp+tn)/(tp+tn+fp+fn)*100
 print("\n{0:<20}{1:<4.1f}%\n".format("Overall Classification Rate: ", p))
@@ -111,13 +82,7 @@ print("{0:<15}{1:<2.0f}% ({2:<}){3:>6.0f}% ({4:<})".format("win", tn/(tn+fn)*100
 print("{0:<16}{1:<1.0f}% ({2:<}){3:>7.0f}% ({4:<}) \n".format("not win", fn/(tn+fn)*100,fn, tp/(tp+fp)*100, tp))
 
 
-# In[16]:
-
-
 test_data['predictions'] = predictions
-
-
-# In[17]:
 
 
 test_data.to_excel('test_data.xlsx', index=False)
